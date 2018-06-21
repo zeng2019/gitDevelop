cd490bc2a5d1640122e3d527f7bdf87938b77f2e
Zeng <wbzeng_hn@163.com>
2018-06-22 00:28:10 +0800

    initial commit

---
+++ starweather/app/src/main/java/cn/edu/haust/starweather/db/Province.java
@@ -0,0 +1,33 @@
+package cn.edu.haust.starweather.db;
+
+public class Province {
+    private int id;
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int provinceCode;
+
+}
